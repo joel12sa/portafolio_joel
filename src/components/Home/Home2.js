0feb35af9463839e6888a23aae82a037c6b215de
joel12sa <sasigjo3190@gmail.com>
2024-11-30 23:25:35 -0500

    Update
    Change in Home and new component Footer

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -96,16 +96,7 @@ function Home2() {
                   <FaLinkedinIn />
                 </a>
               </li>
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/joel.sasig/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
+              
             </ul>
           </Col>
         </Row>    
