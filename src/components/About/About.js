58fb60adc284f364d5006e6d2af8af77745727c0
joel12sa <sasigjo3190@gmail.com>
2024-12-03 19:13:15 -0500

    New section about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,23 +1,44 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import laptopImg from "../../Assests/about.png";
+
 
 function About() {
-    return (
-        <section>
-            <Container fluid className="about-section" id="about">
-                <Container>
-                    <Row>
-                        
+  return (
+    <Container fluid className="about-section">
+      <Container>
+        <Row style={{ justifyContent: "center", padding: "10px" }}>
+          <Col
+            md={7}
+            style={{
+              justifyContent: "center",
+              paddingTop: "30px",
+              paddingBottom: "50px",
+            }}
+          >
+            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              Know Who <strong className="purple">I'M</strong>
+            </h1>
+          </Col>
+          <Col
+            md={5}
+            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            className="about-img"
+          >
+            <img src={laptopImg} alt="about" className="img-fluid" />
+          </Col>
+        </Row>
+        <h1 className="project-heading">
+          Professional <strong className="purple">Skillset </strong>
+        </h1>
 
-                    </Row>
-                
-                
-                </Container>
 
-            </Container>
-        </section>
-        
-    );
-    
+        <h1 className="project-heading">
+          <strong className="purple">Tools</strong> I use
+        </h1>
+      </Container>
+    </Container>
+  );
 }
+
 export default About;
